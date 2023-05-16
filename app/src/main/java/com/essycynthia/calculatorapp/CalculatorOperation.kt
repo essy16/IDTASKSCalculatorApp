e5e24bbd3bd1e6ec3b48a6a08286d1e78438b1ad
“Esther <essyc14@gmail.com>
2023-05-16 13:35:40 +0300

    first commit

---
+++ app/src/main/java/com/essycynthia/calculatorapp/CalculatorOperation.kt
@@ -0,0 +1,9 @@
+package com.essycynthia.calculatorapp
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add : CalculatorOperation("+")
+    object Multiply : CalculatorOperation("*")
+    object Divide : CalculatorOperation("/")
+    object Subtract : CalculatorOperation("-")
+
+}
---
+++ app/src/main/java/com/essycynthia/calculatorapp/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.essycynthia.calculatorapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Bright_purple = Color(0xFFBF40BF)
+val Light_Violet = Color(0xFFCF9FFF)
+val Cadmium_Yellow = Color(0xFFFFFF8F)
+val peach = Color(0xFFFFE5B4)
